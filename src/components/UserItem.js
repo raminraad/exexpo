44dd20343946f63fcad6153757c222cb0c445e20
Ramin Raad <RaminRaad@outlook.com>
2020-05-08 21:14:33 +0430

    implement swipe to left on list item of UserItem.js

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,13 +1,10 @@
-import { ListItem, Avatar } from "react-native-elements";
-import React, { Component } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { TouchableWithoutFeedback } from "react-native-gesture-handler";
-// import IconFeather from 'react-native-vector-icons/Feather';
+import {ListItem, Avatar} from "react-native-elements";
+import React, {Component} from "react";
+import {View, Text, StyleSheet, Image} from "react-native";
+import {TouchableWithoutFeedback} from "react-native-gesture-handler";
+import IconFeather from 'react-native-vector-icons/Feather';
 import {Icon} from 'react-native-elements';
-// import {GestureHandler} from "expo/build/removed.web";
-//
-// const {Swipeable} = GestureHandler;
-
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export default class UserItem extends Component {
     state = {
@@ -15,7 +12,7 @@ export default class UserItem extends Component {
     };
 
     onPerss = () => {
-        this.setState((prevState, preveProps) => ({ isSelected: !prevState.isSelected }));
+        this.setState((prevState, preveProps) => ({isSelected: !prevState.isSelected}));
     };
 
     renderDetails = () => (
@@ -24,32 +21,43 @@ export default class UserItem extends Component {
         </View>
     );
 
-    render() {
-        const { isSelected } = this.state;
+    LeftAction = () => {
         return (
-            <View >
-                <TouchableWithoutFeedback onPress={this.onPerss}>
-                    <View style={styles.container}>
-                        <ListItem subtitle={this.props.item.jobTitle}
-                            title={this.props.item.fullname}
-                            rightAvatar={{ source: { uri: this.props.item.avatar_url }, size: 48 }}
-                            style={{ flex: 0.99, marginVertical: 0, padding: 0 }} />
-                        {/* <IconFeather
-                            name={isSelected ? "chevron-up" : "chevron-down"}
-                            type='evilicon'
-                            color={'#006494'}
-                            size={32} /> */}
+            <View style={styles.leftAction}>
+
+                <IconFeather
+                    onPress={() => alert('جستجو')}
+                    color='white'
+                    name={"trash"}
+                    size={32}/>
+
+            </View>
+        )
+    };
+
+    render() {
+        const {isSelected} = this.state;
+        return (
+            <Swipeable renderLeftActions={this.LeftAction}>
+                <View>
+                    <TouchableWithoutFeedback onPress={this.onPerss}>
+                        <View style={styles.container}>
+                            <ListItem subtitle={this.props.item.jobTitle}
+                                      title={this.props.item.fullname}
+                                      rightAvatar={{source: {uri: this.props.item.avatar_url}, size: 48}}
+                                      style={{flex: 0.99, marginVertical: 0, padding: 0}}/>
                             <Icon
                                 reverse
                                 name={isSelected ? "chevron-up" : "chevron-down"}
                                 type='font-awesome'
                                 size={12}
-                                color={isSelected?'#EE6C4D':'#979DAC'}
-                                />
-                    </View>
-                </TouchableWithoutFeedback>
-                {isSelected && this.renderDetails()}
-            </View>
+                                color={isSelected ? '#EE6C4D' : '#979DAC'}
+                            />
+                        </View>
+                    </TouchableWithoutFeedback>
+                    {isSelected && this.renderDetails()}
+                </View>
+            </Swipeable>
         )
     }
 }
@@ -69,19 +77,29 @@ const styles = StyleSheet.create(
             flexDirection: 'row-reverse',
             justifyContent: 'flex-start',
             alignItems: 'center',
-            backgroundColor: '#2B2D42',
+            backgroundColor: '#2a9d8f',
             marginHorizontal: 10,
             marginTop: 0,
-            padding:15,
+            padding: 15,
             borderRadius: 5
         },
         detailsText: {
             fontSize: 16,
-            textAlign:'right',
+            textAlign: 'right',
             paddingHorizontal: 35,
-            color:'#EDF2F4'
+            color: '#EDF2F4'
+        },
+        leftAction: {
+            flex:.1,
+            justifyContent: 'center',
+            alignItems:'center',
+            flexDirection:'row',
+            backgroundColor:'#EF233C',
+            marginTop: 10,
+            marginLeft: 10,
+            borderRadius: 5,
         }
     }
-)
+);
 
 

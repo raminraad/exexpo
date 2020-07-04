b269b1347132a822ceb12c111f110b2a806733d9
raad <raminraad@outlook.com>
2020-07-04 11:44:23 +0430

    camera form preview progress

--- src/components/AddProductFromCamera.js
+++ src/components/AddProductFromCamera.js
@@ -4,12 +4,14 @@ import { Camera } from "expo-camera";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import * as rxGlobal from "../lib/rxGlobal";
 import { Image } from "react-native-elements";
+import { Spinner } from "native-base";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [isOnPreview, setIsOnPreview] = useState(false);
+  const [isCapturing, setIsCapturing] = useState(false);
   const [pictures, setPictures] = useState([]);
   useEffect(() => {
     (async () => {
@@ -48,7 +50,14 @@ export default function App() {
             }}>
             <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}> Flip </Text>
           </TouchableOpacity>
-          <TouchableOpacity
+          {isCapturing?<Spinner size={72} color='white' style={[
+              rxGlobal.globalStyles.CircleShapeView,
+              {
+                backgroundColor: "#FFFFFF77",
+                marginBottom: 20,
+                alignSelf: "center",
+              },
+            ]}/>: <TouchableOpacity
             style={[
               rxGlobal.globalStyles.CircleShapeView,
               {
@@ -58,23 +67,30 @@ export default function App() {
               },
             ]}
             onPress={async () => {
-              if (cameraRef) {
-                let newPicture = await cameraRef.takePictureAsync();
-                setPictures([...pictures, newPicture]);
-                setIsOnPreview(true);
-                console.log("photo", newPicture);
+              try {
+                setIsCapturing(true);
+                if (cameraRef) {
+                  let newPicture = await cameraRef.takePictureAsync();
+                  setPictures([...pictures, newPicture]);
+                  setIsOnPreview(true);
+                  console.log("photo", newPicture);
+                }
+              } catch (err) {
+                
+              } finally {
+                setIsCapturing(false);
               }
             }}>
             <MaterialCommunityIcons name='camera-iris' size={72} color='white' />
-          </TouchableOpacity>
+          </TouchableOpacity>}
         </View>
       </Camera>
 
-      <ScrollView horizontal style={{flex:1, marginVertical: 10 }}>
+      {pictures.length?<ScrollView horizontal style={{flex:1, marginVertical: 10 }}>
         {pictures.map((pic) => (
           <Image source={{ uri: pic.uri }} style={{marginHorizontal:5, width: 100, height: 100 }} PlaceholderContent={<ActivityIndicator />} key={pic.uri} />
         ))}
-      </ScrollView>
+      </ScrollView>:null}
     </View>
   );
 }
